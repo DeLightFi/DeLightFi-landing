057873db532b187c7ce60efcb2c2c743a34d68c1
Mathieu M <85166679+Mathaamore@users.noreply.github.com>
2023-02-09 23:13:51 +0100

    3divs2

---
+++ src/components/ThreeDivs2/ThreeDivs2Elements.js
@@ -0,0 +1,181 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 60px);
+  font-family: "Spline Sans", sans-serif;
+  color: white;
+  font-size: 16px;
+  background-color: black;
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - 2 * 6%);
+  height: calc(100% - 2 * 3vw);
+  padding: 3vw 6%;
+  gap: 30px;
+`;
+
+export const Head = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+export const Text = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 3%;
+`;
+
+export const Title = styled.div`
+  font-family: "Spline Sans Mono";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 3.6vw;
+  line-height: 3.8vw;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+`;
+
+export const SubTitle = styled.div`
+  width: 100%;
+  font-family: "Spline Sans Mono";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1vw;
+  line-height: 1.4vw;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+`;
+
+export const BoxWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  gap: 2%;
+  align-items: flex-end;
+`;
+
+export const LBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  height: 100%;
+  width: 30%;
+
+  font-family: "Spline Sans Mono";
+  font-style: normal;
+  font-weight: 700;
+  color: #ffffff;
+
+  background: #161616;
+  border-radius: 20px;
+
+  h1 {
+    font-size: 2.2vw;
+    margin: 0 10%;
+    color: #fab52e;
+  }
+
+  h2 {
+    font-size: 1.6vw;
+    margin: 0 10%;
+  }
+
+  span {
+    font-weight: 600;
+    font-size: 1.2vw;
+    margin: 0 10%;
+  }
+`;
+
+export const RBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 70%;
+  height: 100%;
+
+  gap: 6%;
+`;
+
+export const RBox1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  height: 40%;
+  width: 100%;
+
+  font-family: "Spline Sans Mono";
+  font-style: normal;
+  font-weight: 700;
+  color: #ffffff;
+
+  background: #161616;
+  border-radius: 20px;
+
+  h1 {
+    font-size: 2.2vw;
+    margin: 0 10%;
+    color: #fab52e;
+  }
+
+  h2 {
+    font-size: 1.6vw;
+    margin: 0 10%;
+  }
+
+  span {
+    font-weight: 600;
+    font-size: 1.2vw;
+    margin: 0 10%;
+  }
+`;
+
+export const RBox2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  height: 60%;
+  width: 100%;
+
+  font-family: "Spline Sans Mono";
+  font-style: normal;
+  font-weight: 700;
+  color: #ffffff;
+
+  background: #161616;
+  border-radius: 20px;
+
+  h1 {
+    font-size: 2.2vw;
+    margin: 0 10%;
+    color: #fab52e;
+  }
+
+  h2 {
+    font-size: 1.6vw;
+    margin: 0 10%;
+  }
+
+  span {
+    font-weight: 600;
+    font-size: 1.2vw;
+    margin: 0 10%;
+  }
+`;
---
+++ src/components/ThreeDivs2/index.js
@@ -0,0 +1,62 @@
+import {
+  Head,
+  Text,
+  SubTitle,
+  Title,
+  Content,
+  Wrapper,
+  BoxWrapper,
+  LBox,
+  MBox,
+  RBox,
+  RBoxWrapper,
+  RBox1,
+  RBox2,
+} from "./ThreeDivs2Elements";
+
+const ThreeDivs2 = () => {
+  return (
+    <Wrapper id="about">
+      <Content>
+        <Head>
+          <Text>
+            <Title>How it works?</Title>
+            <SubTitle>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </SubTitle>
+          </Text>
+        </Head>
+        <BoxWrapper>
+          <LBox>
+            <h2>Lorem ipsum</h2>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              volutpat bibendum fermentum. Fusce in sapien sit amet lectus
+              iaculis facilisis.
+            </span>
+          </LBox>
+          <RBoxWrapper>
+            <RBox1>
+              <h2>Lorem ipsum</h2>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                volutpat bibendum fermentum. Fusce in sapien sit amet lectus
+                iaculis facilisis.
+              </span>
+            </RBox1>
+            <RBox2>
+              <h2>Lorem ipsum</h2>
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+                volutpat bibendum fermentum. Fusce in sapien sit amet lectus
+                iaculis facilisis.
+              </span>
+            </RBox2>
+          </RBoxWrapper>
+        </BoxWrapper>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default ThreeDivs2;
