0ff1cfc15dcfdff42b0bc768366d39cdd15cc32b
Mathieu M <85166679+Mathaamore@users.noreply.github.com>
2023-02-09 23:25:36 +0100

    change section

--- src/components/Section/SectionElements.js
+++ src/components/Section/SectionElements.js
@@ -3,13 +3,13 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
-  align-items: center;
   width: calc(100% - 2 * 40px);
-  height: calc(100vh - 2 * 40px);
+  height: calc(100vh - 2 * 40px - 60px);
   padding: 40px;
   gap: 30px;
   font-family: "Spline Sans", sans-serif;
   color: white;
   font-size: 16px;
+
+  background: #ffa72e;
 `;
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,29 +1,7 @@
-import MagicCard from "../MagicCard";
 import { Wrapper } from "./SectionElements";
 
 const Section = () => {
-  return (
-    <Wrapper id="section">
-      <MagicCard
-        name="card1"
-        img={
-          "https://images.ctfassets.net/6g6hg01fg28j/3BziIPFoVB1q4z6V1CwbZc/0d11f473f2d914687d62e31685410571/illustration_starknet-image.png"
-        }
-      />
-      <MagicCard
-        name="card2"
-        img={
-          "https://images.ctfassets.net/6g6hg01fg28j/3BziIPFoVB1q4z6V1CwbZc/0d11f473f2d914687d62e31685410571/illustration_starknet-image.png"
-        }
-      />
-      <MagicCard
-        name="card3"
-        img={
-          "https://images.ctfassets.net/6g6hg01fg28j/3BziIPFoVB1q4z6V1CwbZc/0d11f473f2d914687d62e31685410571/illustration_starknet-image.png"
-        }
-      />
-    </Wrapper>
-  );
+  return <Wrapper id="section"></Wrapper>;
 };
 
 export default Section;
